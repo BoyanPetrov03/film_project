a0a050a2438a42fb5d190eb777f2ff6759654fe3
BoyanPetrov03 <59579042+BoyanPetrov03@users.noreply.github.com>
2024-02-09 14:07:30 +0200

    updated details page

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // Add a click event listener to the icon
+  $(".material-symbols-outlined").click(function () {
+    // Toggle the visibility of the account menu div
+    $("#account-menu").toggle();
+  });
+});
