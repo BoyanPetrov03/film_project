29ae625ae7c6013d51fad75190268c9826bacaa9
KameliaTsankova <kamitsankova@gmail.com>
2024-02-12 06:50:32 +0200

    неща за регистрация
    от друг проект са така че ако можеш да ги вържеш ще е супер и в условието искат да има отделен json file за филмите и не съм сиг това какво е. има и други неща ама засега това

---
+++ login.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+    const registerButton = document.querySelector("button:nth-child(1)");
+    registerButton.addEventListener("click", () => {
+        window.location.href = "/register.html"
+    })
+    const loginButton = document.querySelector("button");
+    const form=document.querySelector(`form`);
+    const pass=document.querySelector(`input[type="password"]`);
+    const email=document.querySelector(`input[name="email"]`);
+    const error=document.querySelector(`span.register-error`);
+    form.onsubmit=(event)=>{
+        event.preventDefault();
+    }
+    loginButton.addEventListener("click", () => {
+        if(!email.value.includes('@')){
+            email.classList.add("invalid-field");
+            error.innerHTML="Invalid email format"
+            return;
+        }
+        fetch("accounts.json")
+        .then(data=>data.json())
+        .then(accounts=>{
+            console.log(accounts)
+            const acc=accounts.find(acc=>acc.email==email.value);
+            if(acc){
+                if(acc.pass==pass.value){
+                    window.location.href="/home.html"
+                }
+                else{
+                    pass.classList.add("invalid-field");
+                    error.innerHTML="Wrong password"
+                }
+            }
+            else{
+                email.classList.add("invalid-field");
+                error.innerHTML="There is no registered user with that email"
+                return;
+            }
+        })
+    })
+}
